82235e0d92f76e1aae4db42114d2aa999940f19a
an.orsich <an.orsich@gmail.com>
2012-07-06 18:22:30 +0300

    comments for ITransformer. Anyone can add new engines by implementing this interface

--- source/Setty/ITransformer.cs
+++ source/Setty/ITransformer.cs
@@ -8,8 +8,20 @@ namespace Setty
 {
     public interface ITransformer
     {
+        /// <summary>
+        /// Transformer accept path to the config file, transform it with key/value settings
+        /// and save result to the output config file.
+        /// Anyone can add new transformer by implementing this interface and adding new search files 
+        /// to the SettyConstants.SearchConfigsNames. Setty will automatically choose transformer by ConfigExtention
+        /// </summary>
+        /// <param name="inputFilePath">Path to the setty config file (xslt or cshtml)</param>
+        /// <param name="outputFilePath">Path to the output file </param>
+        /// <param name="settings">Key/Value settings</param>
         void Transform(String inputFilePath, String outputFilePath, KeyValueConfigurationCollection settings);
 
+        /// <summary>
+        /// Extention of transformer config file
+        /// </summary>
         string ConfigExtention { get; }
     }
 }
